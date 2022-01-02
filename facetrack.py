b4b142680b22bde4002c4ac1bb9ddb6260f84506
Rohan Bansal <bansalrohan18@gmail.com>
2022-01-01 22:39:39 -0800

    working facetracking

---
+++ facetrack.py
@@ -0,0 +1,67 @@
+import numpy as np
+import serial
+import time
+import sys
+import cv2
+
+arduino = serial.Serial(port="/dev/ttyACM0", baudrate=115200)
+arduino.reset_input_buffer()
+arduino.reset_output_buffer()
+
+print("Connected to arduino...")
+
+face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+print("Created classifier...")
+cap = cv2.VideoCapture(1)
+
+def set_res(cap, x,y):
+    cap.set(cv2.CAP_PROP_FRAME_WIDTH, int(x))
+    cap.set(cv2.CAP_PROP_FRAME_HEIGHT, int(y))
+
+frame_w = 640
+frame_h = 480
+set_res(cap, frame_w,frame_h)
+
+while(True):
+    ret, frame = cap.read()
+    cap.read()
+  
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    faces = np.array([]) 
+    faces = face_cascade.detectMultiScale(
+        gray,   
+        scaleFactor=1.3,
+    )
+    
+    for (x, y, w, h) in faces:
+        cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
+
+    cv2.imshow('frame', frame)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+
+    if ([i for i in faces]):                                     
+        face_center_x = faces[0,0]+faces[0,2]/2
+        face_center_y = faces[0,1]+faces[0,3]/2
+        # data = "X{0:.3f}Y{1:.3f}Z".format(face_center_x, face_center_x)
+        # print(data)
+        # arduino.write(data.encode())
+
+        err_x = 30*(face_center_x - frame_w/2)/(frame_w/2)
+        err_y = 30*(face_center_y - frame_h/2)/(frame_h/2)
+        data = "{:.3f}X{:.3f}Y".format(err_x, err_y)
+        print(data)
+        arduino.write(bytes(data, 'utf-8'))
+        # arduino.write((str(err_x) + "x!").encode())        
+        # arduino.write((str(err_y) + "y!").encode())        
+        # print("X: ",err_x," ","Y: ",err_y)
+    else:
+        pass
+        # arduino.write("o!".encode())  
+              
+                     
+arduino.close()
+cap.release()
+cv2.destroyAllWindows()
--- main.py
+++ main.py
@@ -1,1 +1,50 @@
-import serial
+import serial, time
+
+arduino = serial.Serial(port="/dev/ttyACM0", baudrate=115200, timeout=0.1)
+
+def write_read(x):
+    arduino.write(bytes(x, 'utf-8'))
+    time.sleep(0.05)
+    data = arduino.readline()
+    return data
+
+# while True:
+#     if keyboard.is_pressed('left'):
+#         value = write_read("1")
+#     elif keyboard.is_pressed('right'):
+#         value = write_read("2");
+    
+#     if keyboard.is_pressed('up'):
+#         value = write_read("3")
+#     elif keyboard.is_pressed('down'):
+#         value = write_read("4")
+
+import pygame, sys
+import pygame.locals
+
+pygame.init()
+BLACK = (0,0,0)
+WIDTH = 1366
+HEIGHT = 768
+
+windowSurface = pygame.display.set_mode((WIDTH, HEIGHT), 0, 32)
+windowSurface.fill(BLACK)
+pygame.display.set_caption('Nerf Turret')
+
+while True:
+    for event in pygame.event.get():
+
+        if event.type == pygame.locals.QUIT:
+            pygame.quit()
+            sys.exit()
+        elif event.type == pygame.locals.KEYDOWN:
+            if event.key == pygame.K_LEFT: 
+                value = write_read("1")
+            elif event.key == pygame.K_RIGHT:
+                value = write_read("2")
+            if event.key == pygame.K_UP:
+                value = write_read("3")
+            elif event.key == pygame.K_DOWN:
+                value = write_read("4")
+            if event.key == pygame.K_r:
+                value = write_read("-1")
